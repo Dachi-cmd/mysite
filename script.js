edf5c1dd70f67b736d3fb0c9bcf87bfafe935a07
dachi-cmd <dachitate10@gmail.com>
2025-06-10 16:57:25 +0400

    published

---
+++ script.js
@@ -0,0 +1,7 @@
+ const tabButtons = document.querySelectorAll('.tab-btn');
+    tabButtons.forEach(btn => {
+      btn.addEventListener('click', () => {
+        tabButtons.forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+      });
+    });
