c9d721081219cedd55e96f864291bd50ed756120
dachi-cmd <dachitate10@gmail.com>
2025-06-13 21:49:48 +0400

    changed stuff again

--- myapp/core/admin.py
+++ myapp/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import Planet
+
+class PlanetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number_of_days', 'distance_from_sun', 'has_rings')
+    search_fields = ('name',)
+    list_filter = ('has_rings',)
+
+admin.site.register(Planet, PlanetAdmin)
+
--- myapp/core/models.py
+++ myapp/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Planet(models.Model):
+    name = models.CharField(max_length=100)
+    number_of_days = models.IntegerField(help_text="Number of days in a year")
+    distance_from_sun = models.FloatField(help_text="Distance in million km")
+    has_rings = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
